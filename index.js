454fe066c3e572621e82640aa07d73cb15b53a41
Tolu Akinrinola <remmakins@gmail.com>
2023-04-17 02:07:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+    
+    function addingEventListener(){
+    
+    alert('I was clicked!');
+    button.addEventListener('click', addingEventListener);
+    }
+    addingEventListener()
+
 }
